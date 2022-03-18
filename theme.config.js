85dbf3ae74497731f199c7cc51ce6395f93faa03
nyt92 <chamroeunlinda12343@gmail.com>
2022-03-18 19:09:19 +0700

    Update

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/NYT92/haniapi-docs',
   docsRepositoryBase: 'https://github.com/NYT92/haniapi-docs/blob/master',
-  titleSuffix: ' – HANI API',
+  titleSuffix: ' – HaniAPI Docs',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Hanime API</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-      Unofficial API
+      Unofficial API Documentation
       </span>
     </>
   ),
@@ -16,8 +16,8 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Hanime API : Unofficial API" />
-      <meta name="og:description" content="Hanime API : Unofficial API" />
+      <meta name="description" content="Hanime API : Unofficial API Documentation" />
+      <meta name="og:description" content="Hanime API : Unofficial API Documentation" />
       <meta name="og:title" content="Hanime API : Unofficial API"/>
       <meta name="og:image" content="/fav.png" />
       <link rel="icon" type="image/png" sizes="360x360" href="/fav.png"/>
@@ -28,6 +28,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © NYT92 / Templete by Nextra.</>,
+  footerText: <>{new Date().getFullYear()} © NYT92 / nsDev</>,
   unstable_faviconGlyph: '👋',
 }
